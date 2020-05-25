70ad56816da6d9ea9e1201f177d7159f6125cde5
Jael Pati <jael.pati@gmail.com>
2020-05-25 20:35:52 +0200

    ajout des reves sur la carte

---
+++ src/data.js
@@ -0,0 +1,37 @@
+const data = [
+    {
+        id: 1,
+        imagePath: "images/maldives.jpg",
+        description: "Maison sur pilotis aux Maldives",
+        done: false,
+        link: "http://jael-developpeur.fr",
+        coordinates: {
+           lat : 41.00865280813229,
+           lng : 28.980106601743014,
+        }
+    },
+    {
+        id: 2,
+        imagePath: "images/paris.jpg",
+        description: "Maison sur pilotis aux Maldives",
+        done: false,
+        link: "http://jael-developpeur.fr",
+        coordinates: {
+           lat : 41.89027407375351,
+           lng : 12.492037779047413,
+        }
+    },
+    {
+        id: 3,
+        imagePath: "images/kenya.jpg",
+        description: "Maison sur pilotis aux Maldives",
+        done: true,
+        link: "http://jael-developpeur.fr",
+        coordinates: {
+           lat : 50.275241417772584,
+           lng : 3.350955757218638,
+        }
+    },
+]
+
+export {data}
